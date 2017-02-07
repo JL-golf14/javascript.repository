3a3d42196126b0fea2809f226f2df1ece2317f79
Linh Kieu Le <linhkieule@Linhs-MacBook-Pro-2.local>
2017-02-07 17:03:19 -0600

    first commit

---
+++ client.js
@@ -0,0 +1,97 @@
+// var zombieOne = 'Luke'
+// var zombieTwo = 'Paul'
+// var zombieThree = 'Fred'
+//
+//
+// var zombieArray=['Kris', zombieTwo, 'Catherine', 'Jeremy'];
+// var zombieArrayTwo= ['Rabbit', 'Cow'];
+//
+// console.log(zombieArray);
+// zombieArray.push(zombieOne);
+// zombieArray.push(zombieTwo);
+// zombieArray.push(zombieThree);
+//
+// console.log(zombieArray);
+// zombieArray.pop();
+// console.log(zombieArray);
+//
+// var whoGotPopped = zombieArray.shift();
+// console.log(whoGotPopped);
+// console.log(zombieArray.length);
+//
+// zombieArray[2]='Steve';
+// zombieArray[3]='Carla';
+// zombieArray[8]='Cody';
+//
+// console.log(zombieArray.);
+// console.log(zombieArray.length);
+// for (var i = 0; i < zombieArray.length; i++) {
+//   console.log(zombieArray[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+// console.log(whoIsAZombie(zombieArray));
+//
+// function whoIsAZombie(zombies){
+// // console.log("zombies: ",zombies);
+// // console.log("zombies[0] ",zombies[0,2]);//bracket notation OR index notation
+// return zombies[0]+" and " +zombies[1];
+// }
+// var array1=[1,2,3,4,5];
+// var array2=[6,7,8,9,10,11,12]
+// function checkOne(arrayCheck){
+//   return arrayCheck[0]+arrayCheck[3];
+//
+// };
+// console.log(checkOne(array1));
+// console.log(checkOne(array2));
+// console.log(array2);
+
+
+var ballOfString = "yarn";
+
+var firstLetter = ballOfString.charAt(0);   // or ballOfString[0]
+
+var stringLength = ballOfString.length;   //   =4
+
+var firstTwoLeters = ballOfString.substring(0,2); // ya
+
+
+var lastTwoLetters = ballOfString.substring(stringLength-2, stringLength);
+    //lastTwoLetters = ballOfString.substring(2,4); //
+    // lastTwoLetters = ballOfString.substring(stringLength-2, stringLength);
+                                                        // rn
+
+var middleTwoLetters = ballOfString.substring(stringLength / 2 - 2, stringLength / 2 -1 );
+console.log(middleTwoLetters);
+
+var commaSeperatedValues = "one,two,three,four";
+var newArray = commaSeperatedValues.split(',');
+
+console.log(newArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
